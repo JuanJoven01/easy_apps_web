866fe507a91e068efcdb41e5a13d8e916935b474
JuanJoven01 <juanjov95@hotmail.com>
2025-02-04 18:47:11 -0500

    feat(app - components): navbar
    Add a navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Navbar from './Components/navbar'
+
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Navbar />
+      <div className='total flex bg-slate-900'>
+            <div className='semi-total w-full max-w-6xl mx-auto'>
+            </div>
+          </div>
     </>
   )
 }
---
+++ src/Components/navbar/index.jsx
@@ -0,0 +1,21 @@
+
+const Navbar = () => {
+
+
+    return(
+        <nav className=" border-b-2 border-slate-400 flex h-16 justify-between bg-slate-900 bg-opacity-20 backdrop-blur-md fixed w-full text-slate-200 font-lora z-10"> 
+            <p className='my-auto ml-5'>EASY APPS</p>
+            <ul className="my-auto mr-5 flex">
+                <a className={`px-5 hover:cursor-pointer hover:text-echo hover:underline`} href="https://www.linkedin.com/in/juan-pablo-joven-urbano-05202129a/" target="_blank" rel="noopener noreferrer" >
+                    Linkedin
+                </a>
+                <a className={`px-5 hover:cursor-pointer hover:text-echo hover:underline`} href="https://github.com/JuanJoven01" target="_blank" rel="noopener noreferrer" >
+                    Github
+                </a>
+            </ul>
+        </nav>
+    )
+    
+}
+
+export default Navbar
