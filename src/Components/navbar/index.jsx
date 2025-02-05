b99262efda0fdbffa1075a973dbb3bf2daad47df
JuanJoven01 <juanjov95@hotmail.com>
2025-02-05 11:46:32 -0500

    feat(navbar): background
    Removes background color and set transparency.

--- src/Components/navbar/index.jsx
+++ src/Components/navbar/index.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
 
 
     return(
-        <nav className=" border-b-2 border-slate-400 flex h-16 justify-between bg-slate-900 bg-opacity-20 backdrop-blur-md fixed w-full text-slate-200 font-lora z-10"> 
+        <nav className=" border-b-2 border-slate-400 flex h-16 justify-between bg-opacity-20 backdrop-blur-md fixed w-full text-slate-200 font-lora z-10"> 
             <p className='my-auto ml-5'>EASY APPS</p>
             <ul className="my-auto mr-5 flex">
                 <a className={`px-5 hover:cursor-pointer hover:text-echo hover:underline`} href="https://www.linkedin.com/in/juan-pablo-joven-urbano-05202129a/" target="_blank" rel="noopener noreferrer" >
